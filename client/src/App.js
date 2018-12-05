0ae15ee9b8f512d0de98be250808e9da2ea49169
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-12-05 15:16:16 -0600

    adding class components for Login/Registration
    also had to do small set ups for React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './Components/LandingPage.js';
+import RegistrationPage from './Components/RegistrationPage.js';
+import LoginPage from './Components/LoginPage.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route exact path="/" component={ LandingPage } />
+        <Route path="/login" component={ LoginPage } />
+        <Route path="/registration" component={ RegistrationPage } />
       </div>
     );
   }
