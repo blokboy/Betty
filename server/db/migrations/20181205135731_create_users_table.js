d5c9a3630cfe3b02bf726c1cdf28378d3ce1c8f0
Allen Hai <allenhai03@gmail.com>
2018-12-17 14:18:09 -0600

    add email field to user schema, and make category field optional for todo schema, write Todo model functions

--- server/db/migrations/20181205135731_create_users_table.js
+++ server/db/migrations/20181205135731_create_users_table.js
@@ -1,6 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', t => {
-    t.increments()
+    t.increments('id')
+    t.string('email', 128).notNullable()
     t.string('username', 16).notNullable()
     t.string('phone_number', 16).notNullable()
     t.timestamp('created_at').defaultTo(knex.fn.now())
--- server/db/migrations/20181205140648_create_todos_table.js
+++ server/db/migrations/20181205140648_create_todos_table.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('todos', t => {
-    t.increments()
+    t.increments('id')
     t.string('title', 32).notNullable()
     t.string('content', 255).notNullable()
-    t.string('category', 16).notNullable()
+    t.string('category', 16)
     t.integer('user_id')
       .unsigned()
       .references('id')
